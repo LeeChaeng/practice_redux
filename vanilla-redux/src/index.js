730bea8c00c59acb10c779bf931358f2d1930802
LeeChaeng <htijl@naver.com>
2021-01-26 14:21:11 +0900

    Subscriptions

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -4,12 +4,31 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = "0";
+
 const countModifier = (count = 0, action) => {
   if (action.type === "ADD") {
-    console.log("it's add!");
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
   }
-  return count;
 };
 const countStore = createStore(countModifier);
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
 
-countStore.dispatch({ type: "ADD" });
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
