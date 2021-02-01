ac04eb082c78f530133b5281ecd73bd8a4e3d140
LeeChaeng <htijl@naver.com>
2021-02-01 12:37:10 +0900

    mapStateToProps

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./component/App";
+import store from "./store";
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App></App>
   </Provider>,
   document.getElementById("root")
